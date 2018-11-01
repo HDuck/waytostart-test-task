8adce1402b8014e497102fd260884d75f1242c0d
HDuck <1ntranc3d@gmail.com>
2018-11-01 20:41:02 +0300

    fixes

---
+++ src/vehicle-form/vehicle-form.js
@@ -0,0 +1,8 @@
+(function vehicleForm() {
+    var inputs = document.getElementsByClassName('vehicle-form__input');
+    var inputsArr = Array.prototype.slice.call(inputs, 0);
+
+    console.log(inputsArr);
+
+
+})()
